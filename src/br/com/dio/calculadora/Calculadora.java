caa40b99e0bc5e1bb7b73625c380c60a2dad1403
Weslei Batista <wesleideveloper@gmail.com>
2021-12-17 16:36:01 -0300

    criação de calculadora

---
+++ src/br/com/dio/calculadora/Calculadora.java
@@ -0,0 +1,45 @@
+package br.com.dio.calculadora;
+
+import java.util.Scanner;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		double a, b;
+
+		System.out.println("Digite o primeiro valor: ");
+		a = scan.nextDouble();
+		System.out.println("Digite o segundo valor: ");
+		b = scan.nextDouble();
+
+		double soma = soma(a, b);
+		double subtracao = subtracao(a, b);
+		double multiplicacao = multiplicacao(a, b);
+		double divisao = divisao(a, b);
+
+		System.out.println("soma: " + soma);
+		System.out.println("subtracao: " + subtracao);
+		System.out.println("multiplicacao: " + multiplicacao);
+		System.out.println("divisao: " + divisao);
+
+	}
+
+	public static double soma(double a, double b) {
+		return a + b;
+	}
+
+	public static double subtracao(double a, double b) {
+		return a - b;
+	}
+
+	public static double multiplicacao(double a, double b) {
+		return a * b;
+	}
+
+	public static double divisao(double a, double b) {
+		return a / b;
+	}
+
+}
