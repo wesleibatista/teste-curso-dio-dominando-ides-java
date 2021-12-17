db0ec6901182993ab1d09818a613b157e9c8faa5
Weslei Batista <wesleideveloper@gmail.com>
2021-12-17 15:24:52 -0300

    first commit

--- src/br/com/dio/model/Gato.java
+++ src/br/com/dio/model/Gato.java
@@ -8,13 +8,14 @@ public class Gato {
 	private String cor;
 	private Integer idade;
 
-	public Gato() {
-	}
 
 	public Gato(String nome, String cor, Integer idade) {
 		this.nome = nome;
 		this.cor = cor;
 		this.idade = idade;
+	}
+
+	public Gato() {
 	}
 
 	public String getNome() {
